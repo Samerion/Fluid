d4452b5f2d09577936770cea5fec98973008584c
Soaku <soaku@samerion.com>
2021-08-27 14:14:34 +0200

    Make GluiOnionFrame respect GluiNode.toRemove

--- source/glui/node.d
+++ source/glui/node.d
@@ -251,6 +251,8 @@ abstract class GluiNode : Styleable {
 
         import std.algorithm : min, max;
 
+        assert(!toRemove, "A toRemove child wasn't removed from container.");
+
         // If hidden, don't draw anything
         if (hidden) return;
 
