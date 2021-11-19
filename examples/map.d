c848fc26503c6cc52ff5a70453e474ad269eda68
Soaku <soaku@samerion.com>
2021-11-19 19:37:02 +0100

    Add anchoring for mapSpace

--- examples/map.d
+++ examples/map.d
@@ -14,6 +14,9 @@ void main() {
 
     root = mapSpace(
         .layout!(1, "fill"),
+        makeTheme!q{
+            GluiFrame.styleAdd.backgroundColor = Color(0xaa, 0xaa, 0xaa, 0xff);
+        },
 
         // A button to toggle overlap
         Vector2(0, 0), button("Toggle overlap", () @trusted {
@@ -21,15 +24,34 @@ void main() {
         }),
 
         // Sample elements
-        Vector2(200, 200), label("Hello, World!"),
-        Vector2(-50, 60),  label("Overlapping label"),
-        Vector2(300, 500), label("Try to resize the window!"),
+        Vector2(100, 200), label("Hello, World!"),
+        Vector2(-50, 30),  label("Overlapping label"),
+
+        // Place one label relative to its end
+        dropVector!("end", "start"),
+        Vector2(0, 100), label("Hidden text"),
+
+        // A centered text
+        Vector2(50, 140), label("Left align"),
+        dropVector!("center", "start"),
+        Vector2(50, 160), label("Center align"),
+
+        // Just an informative label
+        Vector2(100, 550), label("Try to resize the window!"),
+
+        // A little "dropdown" frame
+        dropVector!"auto",
+        Vector2(300, 150),
+        vframe(
+            label("Hello,"),
+            label("World!"),
+            label("How's"),
+            label("it"),
+            label("going"),
+        ),
 
         // We need to go deeper!
-        Vector2(300, 300), vframe(
-            makeTheme!q{
-                GluiFrame.styleAdd.backgroundColor = Color(0xaa, 0xaa, 0xaa, 0xff);
-            },
+        Vector2(300, 400), vframe(
             mapSpace(
                 label("We need"),
                 Vector2(40, 40), label("to go"),
--- source/glui/map_space.d
+++ source/glui/map_space.d
@@ -1,6 +1,8 @@
 module glui.map_space;
 
 import raylib;
+
+import std.conv;
 import std.algorithm;
 
 import glui.node;
@@ -13,35 +15,105 @@ import glui.utils;
 
 alias mapSpace = simpleConstructor!GluiMapSpace;
 
+/// Defines the direction the node is "dropped from", that is, which corner of the object will be the anchor.
+/// Defaults to `start, start`, therefore, the supplied coordinate refers to the top-left of the object.
+///
+/// Automatic may be set to make it present common dropdown behavior — top-left by default, but will change if there
+/// is overflow.
+enum MapDropDirection {
+
+    start, center, end, automatic
+
+}
+
+struct MapDropVector {
+
+    MapDropDirection x, y;
+
+}
+
+struct MapPosition {
+
+    Vector2 coords;
+    MapDropVector drop;
+
+    alias coords this;
+
+}
+
+MapDropVector dropVector()() {
+
+    return MapDropVector.init;
+
+}
+
+MapDropVector dropVector(string dropXY)() {
+
+    return dropVector!(dropXY, dropXY);
+
+}
+
+MapDropVector dropVector(string dropX, string dropY)() {
+
+    enum val(string dropV) = dropV == "auto"
+        ? MapDropDirection.automatic
+        : dropV.to!MapDropDirection;
+
+    return MapDropVector(val!dropX, val!dropY);
+
+}
+
 class GluiMapSpace : GluiSpace {
 
+    alias DropDirection = MapDropDirection;
+    alias DropVector = MapDropVector;
+    alias Position = MapPosition;
+
     /// Mapping of nodes to their positions.
-    Vector2[GluiNode] positions;
+    Position[GluiNode] positions;
 
     /// If true, the node will prevent its children from leaving the screen space.
     bool preventOverlap;
 
     static foreach (index; 0..BasicNodeParamLength) {
 
-        /// Construct the space. Arguments are either nodes, or position vectors affecting all next nodes.
+        /// Construct the space. Arguments are either nodes, or positions/vectors affecting the next node added through
+        /// the constructor.
         this(T...)(BasicNodeParam!index params, T children)
-        if (!T.length || is(T[0] == Vector2) || is(T[0] : GluiNode)) {
+        if (!T.length || is(T[0] == Vector2) || is(T[0] == DropVector) || is(T[0] == Position) || is(T[0] : GluiNode)) {
 
             super(params);
 
-            Vector2 position;
+            Position position;
 
             static foreach (child; children) {
 
                 // Update position
-                static if (is(typeof(child) == Vector2)) {
+                static if (is(typeof(child) == Position)) {
 
                     position = child;
 
                 }
 
+                else static if (is(typeof(child) == MapDropVector)) {
+
+                    position.drop = child;
+
+                }
+
+                else static if (is(typeof(child) == Vector2)) {
+
+                    position.coords = child;
+
+                }
+
                 // Add child
-                else addChild(child, position);
+                else {
+
+                    addChild(child, position);
+                    position = Position.init;
+
+                }
 
             }
 
@@ -50,7 +122,7 @@ class GluiMapSpace : GluiSpace {
     }
 
     /// Add a new child to the space and assign it some position.
-    void addChild(GluiNode node, Vector2 position) {
+    void addChild(GluiNode node, Position position) {
 
         children ~= node;
         positions[node] = position;
@@ -58,7 +130,7 @@ class GluiMapSpace : GluiSpace {
 
     }
 
-    void moveChild(GluiNode node, Vector2 position) {
+    void moveChild(GluiNode node, Position position) {
 
         positions[node] = position;
 
@@ -76,9 +148,11 @@ class GluiMapSpace : GluiSpace {
 
             child.resize(tree, theme, space);
 
-            // Get the children's bottom right corner
-            minSize.x = max(minSize.x, position.x + child.minSize.x);
-            minSize.y = max(minSize.y, position.y + child.minSize.y);
+            // Get the child's end corner
+            const endCorner = getEndCorner(space, child, position);
+
+            minSize.x = max(minSize.x, endCorner.x);
+            minSize.y = max(minSize.y, endCorner.y);
 
         }
 
@@ -88,10 +162,12 @@ class GluiMapSpace : GluiSpace {
 
         drawChildren((child) {
 
-            const position = positions.require(child, Vector2(0, 0));
+            const position = positions.require(child, Position.init);
+            const space = Vector2(inner.w, inner.h);
+            const startCorner = getStartCorner(space, child, position);
 
-            auto x = inner.x + position.x;
-            auto y = inner.y + position.y;
+            auto x = inner.x + startCorner.x;
+            auto y = inner.y + startCorner.y;
 
             if (preventOverlap) {
 
@@ -109,6 +185,44 @@ class GluiMapSpace : GluiSpace {
             child.draw(childRect);
 
         });
+
+    }
+
+    private alias getStartCorner = getCorner!false;
+    private alias getEndCorner   = getCorner!true;
+
+    private Vector2 getCorner(bool end)(Vector2 space, GluiNode child, Position position) {
+
+        Vector2 result;
+
+        // Get the children's corners
+        static foreach (direction; ['x', 'y']) {{
+
+            const pos = mixin("position.coords." ~ direction);
+            const dropDirection = mixin("position.drop." ~ direction);
+            const childSize = mixin("child.minSize." ~ direction);
+
+            const overflow = pos + childSize > mixin("space." ~ direction);
+
+            static if (end)
+            mixin("result." ~ direction) = dropDirection.predSwitch(
+                DropDirection.start,     pos + childSize,
+                DropDirection.center,    pos + childSize/2,
+                DropDirection.end,       pos,
+                DropDirection.automatic, overflow ? pos : pos + childSize,
+            );
+
+            else
+            mixin("result." ~ direction) = dropDirection.predSwitch(
+                DropDirection.start,     pos,
+                DropDirection.center,    pos - childSize/2,
+                DropDirection.end,       pos - childSize,
+                DropDirection.automatic, overflow ? pos - childSize : pos,
+            );
+
+        }}
+
+        return result;
 
     }
 
