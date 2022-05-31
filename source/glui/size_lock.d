Samerion/Fluid

commit 3e5667a52b2b98fb1a5bb5f73add6221b1343cc4

sizeLock first paint behavior bugfix
Closes #44

source/glui/size_lock.d
@@ -70,9 +70,9 @@ class GluiSizeLock(T : GluiNode) : T {
         // Resize
         super.resizeImpl(space);
 
-        // Fill in the space
-        if (limit.x != 0) minSize.x = space.x;
-        if (limit.y != 0) minSize.y = space.y;
+        // Fill in the space we get, unless we don't have enough to place ourselves
+        if (limit.x != 0) minSize.x = max(space.x, minSize.x);
+        if (limit.y != 0) minSize.y = max(space.y, minSize.y);
 
     }
 

examples/showcase_2.d
@@ -347,47 +347,36 @@ GluiSpace sizeLimitExample() {
         hspace(
             .layout!"fill",
 
-            vspace(
-                .layout!(1, "fill"),
+            sizeLock!vframe(
+                .layout!(1, "center"),
+                .sizeLimitX(100),
 
-                sizeLock!vframe(
-                    .layout!"center",
-                    .sizeLimitX(100),
-
-                    label("This node will not be wider than 100 pixels"),
-                ),
+                label("This node will not be wider than 100 pixels"),
             ),
 
-            vspace(
-                .layout!(1, "fill"),
+            sizeLock!vframe(
+                .layout!(1, "center"),
+                .sizeLimitX(300),
 
-                sizeLock!vframe(
-                    .layout!"center",
-                    .sizeLimitX(300),
-
-                    label("Even if smaller than"),
-                    label("the limit, the box will"),
-                    label("expand nicely if it can"),
-                ),
-
+                label("Even if contents are"),
+                label("smaller than the limit,"),
+                label("the box will expand"),
+                label("if it can..."),
             ),
 
-            vspace(
-                .layout!(1, "fill"),
+            sizeLock!hframe(
+                .layout!(1, "center"),
+                .sizeLimitY(50),
 
-                sizeLock!hframe(
-                    .layout!1,
-                    .sizeLimitY(50),
-
-                    label("Look into the code! "),
-                    // Creating size-locked nodes is easy and convenient and works with any node type through a
-                    // template! It accepts any node class or node constructor like hframe.
-                    //
-                    //      sizeLock!GluiLabel
-                    //      sizeLock!label
-                    //      sizeLock!vframe
-                    //      sizeLock!hframe
-                ),
+                label("Look into "),
+                label("the code!"),
+                // Creating size-locked nodes is easy and convenient and works with any node type through a
+                // template! It accepts any node class or node constructor like hframe.
+                //
+                //      sizeLock!GluiLabel
+                //      sizeLock!label
+                //      sizeLock!vframe
+                //      sizeLock!hframe
             ),
 
         ),

source/glui/node.d
@@ -478,7 +478,7 @@ abstract class GluiNode : Styleable {
     /// Get the current style.
     protected abstract const(Style) pickStyle() const;
 
-    /// Get the node position.
+    /// Get the node's position in its space box.
     private Vector2 position(Rectangle space, Vector2 usedSpace) const {
 
         float positionImpl(NodeAlign align_, lazy float spaceLeft) {

commit 1565bd4aca17604b7571fcc34824a8419a642aa1

Bugfix sizeLock allowing overflowing nodes
Primary culprit: scroll nodes.

Regression bug introduced by 3e5667a52b2b98fb1a5bb5f73add6221b1343cc4

source/glui/size_lock.d
@@ -70,9 +70,9 @@ class GluiSizeLock(T : GluiNode) : T {
         // Resize
         super.resizeImpl(space);
 
-        // Fill in the space we get, unless we don't have enough to place ourselves
-        if (limit.x != 0) minSize.x = max(space.x, minSize.x);
-        if (limit.y != 0) minSize.y = max(space.y, minSize.y);
+        // Apply the limit to the resulting value; fill in remaining space if available
+        if (limit.x != 0) minSize.x = max(space.x, min(limit.x, minSize.x));
+        if (limit.y != 0) minSize.y = max(space.y, min(limit.y, minSize.y));
 
     }
 