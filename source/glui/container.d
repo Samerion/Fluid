fe13f6e2bb972fede869457f8982194baf8b052c
Artha <artha@samerion.com>
2022-09-08 14:56:50 +0200

    Add scrollIntoView action & trigger it on focus()

--- source/glui/container.d
+++ source/glui/container.d
@@ -5,6 +5,7 @@ import raylib;
 import glui.node;
 import glui.tree;
 import glui.input;
+import glui.actions;
 
 
 @safe:
@@ -14,5 +15,45 @@ import glui.input;
 ///
 /// See_Also: https://git.samerion.com/Samerion/Glui/issues/14
 interface GluiContainer {
+
+    /// Scroll towards the given children node. This should change the parent's properties (without affecting the child)
+    /// so that the child enters the viewport, or becomes as close to it as possible.
+    /// Params:
+    ///     child     = Node to scroll towards. Must be a direct child of this node.
+    ///     viewport  = Size of the current viewport. The parent should
+    ///     parentBox = This node's current padding box.
+    ///     childBox  = The child's current padding box.
+    /// Returns:
+    ///     Estimated new padding box for the child.
+    /// See_Also:
+    ///     `glui.actions.scrollIntoView` for recursive scrolling via `TreeAction`.
+    Rectangle shallowScrollTo(GluiNode child, Vector2 viewport, Rectangle parentBox, Rectangle childBox);
+
+    /// Set focus on the first available focusable node in this tree.
+    final void focusChild() {
+
+        asNode.focusRecurse();
+
+    }
+
+    final inout(GluiNode) asNode() inout {
+
+        import std.format;
+
+        auto node = cast(inout GluiNode) this;
+
+        assert(node, format!"%s : GluiContainer must inherit from a Node"(typeid(this)));
+
+        return node;
+
+    }
+
+    private final LayoutTree* getTree()
+    out (r; r !is null, "Container needs a resize to associate with a tree")
+    do {
+
+        return asNode.tree;
+
+    }
 
 }
