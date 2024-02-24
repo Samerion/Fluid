2ec1298731595356fabf0ae1345f13d4b8f90357
Artha <artha@samerion.com>
2024-02-24 17:08:31 +0100

    Add content for "Writing forms" example

--- examples/forms.d
+++ examples/forms.d
@@ -9,92 +9,61 @@ import fluid.showcase;
 @safe:
 
 
-Space myExample() {
+@(
+    () => label("Fluid comes with a number of useful nodes for taking input from the user. Aside from basics like "
+        ~ "buttons, there are text boxes, checkboxes, sliders and others."),
+    () => label("You can ask the user for some text using a textInput. Provide a callback if you want to do "
+        ~ "something when the user finishes — by pressing the enter key."),
+)
+Space textExample() {
 
-    Slider!string stringSlider;
-    Label stringSliderLabel;
-
-    auto group = new RadioboxGroup;
+    Label nameLabel;
+    TextInput input;
 
     return vspace(
-
-        fieldSlot!vframe(
-            label("Username"),
-            textInput(),
-        ),
-        fieldSlot!vframe(
-            label("Integer"),
-            intInput(),
-        ),
-        fieldSlot!vframe(
-            label("Real number"),
-            floatInput(),
-        ),
-
-        fieldSlot!hframe(
-            checkbox(),
-            label("Accept my rules"),
-        ),
-        fieldSlot!hframe(
-            checkbox(),
-            label("Accept spam"),
-        ),
-
-        label("Choose the pizza"),
-
-        fieldSlot!hframe(
-            radiobox(group),
-            label("Pizza"),
-        ),
-        fieldSlot!hframe(
-            radiobox(group),
-            label("Frog"),
-        ),
-        fieldSlot!hframe(
-            radiobox(group),
-            label("Island"),
-        ),
-
-        fieldSlot!vframe(
-            .layout!"fill",
-
-            label("Int slider"),
-            slider!int(
-                .layout!"fill",
-                iota(0, 3),
-            ),
-        ),
-        fieldSlot!vframe(
-            .layout!"fill",
-
-            label("Float slider"),
-            slider!float(
-                .layout!"fill",
-                iota(0.0f, 1.0f, 0.1f),
-            ),
-        ),
-        fieldSlot!vframe(
-            .layout!"fill",
-
-            hspace(
-                .layout!"fill",
-                label("Factor slider"),
-                stringSliderLabel = label(
-                    .layout!(1, "end"),
-                    ""
-                ),
-            ),
-
-            stringSlider = slider!string(
-                .layout!"fill",
-                only("Never", "Not likely", "Uncertain", "Likely", "Definitely"),
-                2,
-                delegate {
-                    import std.conv;
-                    stringSliderLabel.text = stringSlider.value.text;
-                }
-            ),
-        ),
+        nameLabel = label("What's your name?"),
+        input = textInput("Your name...", delegate {
+            nameLabel.text = "Hi, " ~ input.value ~ "!";
+        }),
     );
 
 }
+
+@(
+    () => label("It's a good idea to label all inputs to make their purpose clear to the user. Besides placing "
+        ~ "a label next to it, wrap them in a 'FieldSlot' so Fluid can associate them together."),
+    () => label(`Note that while text inputs have a placeholder option like "Your name..." above, it's not `
+        ~ `visible unless the input is empty. Adding both is good practice.`),
+)
+Space fieldExample() {
+
+    return fieldSlot!vframe(
+        label("Name:"),
+        textInput("Your name..."),
+    );
+
+}
+
+@(
+    () => label("'FieldSlot' might not have a noticeable effect at first, but don't disregard it as useless. "
+        ~ "FieldSlot will expand the clickable area of the input to cover the label, making it easier for the "
+        ~ "user to locate and understand your fields. This is especially important for smaller nodes, like "
+        ~ "checkboxes. Try clicking the label text below:"),
+)
+Space checkboxExample() {
+
+    return fieldSlot!hframe(
+        checkbox(),
+        label("I agree to the terms and conditions"),
+    );
+
+}
+
+@(
+    () => label(.warningTheme, "Note: Make sure your fieldSlots only contain a single input node. Placing two "
+        ~ "checkboxes or text inputs inside one might cause unexpected behavior."),
+    () => label("Moreover, 'FieldSlot' might be used by external tools to analyze the content, and for example, "
+        ~ "provide information for screen readers. Fluid doesn't come with such tools at the time of writing, "
+        ~ "but such improvements that might be introduced in the future."),
+)
+void noExample() { }
