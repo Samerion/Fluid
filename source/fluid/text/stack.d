1e01b20ab979690f65e346ac8fd9b9f728beb7e9
Artha <artha@samerion.com>
2024-10-23 14:14:07 +0200

    Implement stack node reuse

--- source/fluid/text/stack.d
+++ source/fluid/text/stack.d
@@ -5,11 +5,17 @@
 /// nodes are reused whenever possible, instead of being reclaimed by the GC.
 module fluid.text.stack;
 
+version (unittest)
+    version = Fluid_EnableStackStatistics;
+
 /// Implementation of a stack optimized to reduce allocations.
 ///
 /// Removing nodes from the stack using `pop` or `clear` will move them to a global stack of "free" nodes. 
 /// The next time an item is added to the stack, it will reuse one of these previously freed nodes. This recycling 
 /// mechanism makes it possible to significantly reduce the number of allocations as the program continues.
+///
+/// If version `Fluid_MemoryStatistics` is set, `Stack` will count the number of total nodes it has allocated per 
+/// type and thread and expose then through `totalNodeCount`.
 struct Stack(T) {
 
     private {
@@ -17,13 +23,43 @@ struct Stack(T) {
         /// All stack nodes that are not currently in use.
         static StackNode!T* _trash;
 
+        version (Fluid_EnableStackStatistics)
+            static int _totalNodeCount;
+
         /// Top of the stack.
         StackNode!T* _top;
+
+        /// Bottom of the stack, used to quickly transfer all nodes to the stack with `clear`.
+        /// Warning: Bottom is never cleared, so it doesn't have to be null when empty.
+        StackNode!T* _bottom;
 
     }
 
     /// The stack cannot be copied. Wrap it in a reference counted stack.
     @disable this(const Stack);
+
+    ~this() {
+        clear();
+    }
+
+    version (Fluid_EnableStackStatistics) {
+
+        /// This special field is only available when version `Fluid_MemoryStatistics` is set. `Stack` will then count 
+        /// the number of total nodes it has allocated per type and thread.
+        /// 
+        /// This count can be reset with `resetNodeCount`.
+        /// 
+        /// Returns: Total number of nodes allocated by the stack.
+        static int totalNodeCount() {
+            return _totalNodeCount;
+        }
+
+        /// Reset `totalNodeCount` back to 0.
+        static void resetNodeCount() {
+            _totalNodeCount = 0;
+        }
+
+    }
 
     /// Returns: True if the stack is empty.
     bool empty() const {
@@ -42,6 +78,11 @@ struct Stack(T) {
     ///     item = Item to add to the top of the stack.
     void push(T item) {
         _top = getNode(_top, item);
+
+        // Mark this as the bottom, if it is so
+        if (_top.next is null) {
+            _bottom = _top;
+        }
     }
 
     /// Remove the item at the top of the stack.
@@ -64,6 +105,24 @@ struct Stack(T) {
 
     }
 
+    /// Empty the stack.
+    ///
+    /// Done automatically when the stack leaves the scope.
+    void clear()
+    in (empty || _bottom.next is null)
+    out (; empty)
+    do {
+
+        // Already empty, nothing to do
+        if (empty) return;
+
+        // Trash the bottom
+        _bottom.next = _trash;
+        _trash = _top;
+        _top = null;
+
+    }
+
     /// Params:
     ///     item = Item that will be pushed to the top of the stack.
     void opOpAssign(string op : "~")(T item) {
@@ -76,6 +135,9 @@ struct Stack(T) {
 
         // Trash is empty, allocate
         if (_trash is null) {
+            version (Fluid_EnableStackStatistics) {
+                _totalNodeCount++;
+            }
             return new StackNode!T(next, item);
         }
 
@@ -98,18 +160,29 @@ private struct StackNode(T) {
 
 }
 
+version (unittest) {
+
+    /// Struct for testing the stack.
+    private struct Test { 
+        int value; 
+        alias value this;
+    }
+
+}
+
+@("Stack functions correctly")
 unittest {
 
-    Stack!int stack;
+    Stack!Test stack;
     stack._trash = null;
     assert(stack.empty);
-    stack.push(1);
+    stack.push(Test(1));
     assert(stack.top == 1);
     assert(stack._top.next is null);
-    stack.push(2);
+    stack.push(Test(2));
     assert(stack.top == 2);
     assert(stack._top.next !is null);
-    stack.push(3);
+    stack.push(Test(3));
     assert(stack.top == 3);
     assert(stack.pop() == 3);
     assert(stack.pop() == 2);
@@ -117,9 +190,107 @@ unittest {
     assert(stack.empty);
 
     assert(stack._trash !is null);
-    assert(*stack.getNode(null, 1) == StackNode!int(null, 1));
-    assert(*stack.getNode(null, 2) == StackNode!int(null, 2));
-    assert(*stack.getNode(null, 3) == StackNode!int(null, 3));
+    assert(*stack.getNode(null, Test(1)) == StackNode!Test(null, Test(1)));
+    assert(*stack.getNode(null, Test(2)) == StackNode!Test(null, Test(2)));
+    assert(*stack.getNode(null, Test(3)) == StackNode!Test(null, Test(3)));
     assert(stack._trash is null);
+
+    assert(Stack!Test.totalNodeCount == 3);
+    Stack!Test.resetNodeCount();
     
+}
+
+@("Stack recycles nodes")
+unittest {
+
+    Stack!Test stack;
+    stack._trash = null;
+    stack ~= Test(1);
+    stack ~= Test(2);
+    stack ~= Test(3);
+    assert(stack._trash is null);
+    assert(stack._bottom !is null);
+    assert(Stack!Test.totalNodeCount == 3);
+
+    auto nodes = [stack._top, stack._top.next, stack._top.next.next];
+
+    auto top = stack._top;
+
+    // Clear to destroy the stack
+    stack.clear();
+    assert(stack.empty);
+    assert(stack._trash is top);
+
+    assert(stack._trash == nodes[0]);
+    assert(stack._trash.next == nodes[1]);
+    assert(stack._trash.next.next == nodes[2]);
+
+    stack ~= Test(4);
+    stack ~= Test(5);
+    stack ~= Test(6);
+    assert(Stack!Test.totalNodeCount == 3);
+    assert(stack._trash is null);
+    assert(stack._top == nodes[2]);
+    assert(stack._top.next == nodes[1]);
+    assert(stack._top.next.next == nodes[0]);
+    assert(stack.pop() == 6);
+    assert(stack.pop() == 5);
+    assert(stack.pop() == 4);
+
+    stack._trash = null;
+    assert(Stack!Test.totalNodeCount == 3);
+    Stack!Test.resetNodeCount();
+
+}
+
+@("Multiple stacks can share resources")
+unittest {
+
+    Stack!Test a;
+    Stack!Test b;
+
+    assert(Stack!Test.totalNodeCount == 0);
+    assert(a._trash is null);
+    assert(b._trash is null);
+
+    a ~= Test(1);
+    a ~= Test(2);
+    b ~= Test(3);
+    b ~= Test(4);
+
+    assert(Stack!Test.totalNodeCount == 4);
+
+    assert(a.pop() == 2);
+    assert(a._trash !is null);
+
+    auto freed = a._trash;
+
+    b ~= Test(5);
+    assert(b._top is freed);
+    assert(a._trash is null);
+    assert(Stack!Test.totalNodeCount == 4);
+
+    {
+        Stack!Test c;
+        c ~= Test(6);
+        c ~= Test(7);
+        c ~= Test(8);
+        c ~= Test(9);
+    }
+    assert(Stack!Test.totalNodeCount == 8);
+    assert(a._trash !is null);
+
+    a.clear();
+    b.clear();
+
+    foreach (i; 10..18) {
+        a ~= Test(i);
+    }
+    assert(Stack!Test.totalNodeCount == 8);
+    a ~= Test(18);
+    assert(Stack!Test.totalNodeCount == 9);
+
+    Stack!Test.resetNodeCount();
+    Stack!Test._trash = null;
+
 }
