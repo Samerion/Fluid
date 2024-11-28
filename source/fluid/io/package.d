51696b3e0d73a81edc16ed345b15517798d971c5
Artha <artha@samerion.com>
2024-11-28 09:33:29 +0100

    Initial CanvasIO design

---
+++ source/fluid/io/package.d
@@ -0,0 +1,11 @@
+///
+module fluid.io;
+
+// public import fluid.io.drag;
+// public import fluid.io.focus;
+// public import fluid.io.hover;
+// public import fluid.io.scroll;
+public import fluid.io.canvas;
+
+public import fluid.future.context : IO, isIO;
+public import fluid.input;
