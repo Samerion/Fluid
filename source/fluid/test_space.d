3ee923d014091e8d3ddff93ad12245e3de731ecc
Artha <artha@samerion.com>
2024-11-30 14:00:41 +0100

    Add a test for treeContext.lockTint

--- source/fluid/test_space.d
+++ source/fluid/test_space.d
@@ -896,3 +896,65 @@ unittest {
     );
     
 }
+
+@("Tint can be locked to prevent changes")
+unittest {
+
+    import fluid.frame;
+    import fluid.style;
+
+    static class LockTint : Space {
+
+        this(Ts...)(Ts args) {
+            super(args);
+        }
+
+        override void drawImpl(Rectangle outer, Rectangle inner) {
+
+            treeContext.lockTint();
+            scope (exit) treeContext.unlockTint();
+
+            super.drawImpl(outer, inner);
+            
+        }
+
+    }
+
+    alias lockTint = nodeBuilder!LockTint;
+
+    auto theme = nullTheme.derive(
+        rule!Frame(
+            Rule.backgroundColor = color("#aaa"),
+            Rule.tint = color("#aaaa"),
+        )
+    );
+
+    Frame[7] frames;
+
+    auto root = testSpace(
+        theme,
+        frames[0] = vframe(
+            frames[1] = vframe(
+                lockTint(
+                    frames[2] = vframe(
+                        frames[3] = vframe(),
+                        frames[4] = vframe(),
+                    ),
+                    frames[5] = vframe(),
+                ),
+                frames[6] = vframe(),
+            ),
+        ),
+    );
+
+    root.drawAndAssert(
+        frames[0].drawsRectangle().ofColor("#717171aa"),
+        frames[1].drawsRectangle().ofColor("#4b4b4b71"),
+        frames[2].drawsRectangle().ofColor("#4b4b4b71"),
+        frames[3].drawsRectangle().ofColor("#4b4b4b71"),
+        frames[4].drawsRectangle().ofColor("#4b4b4b71"),
+        frames[5].drawsRectangle().ofColor("#4b4b4b71"),
+        frames[6].drawsRectangle().ofColor("#3232324b"),
+    );
+    
+}
