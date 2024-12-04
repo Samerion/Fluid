192d752dc765714e557afd06f29048942a629294
Artha <artha@samerion.com>
2024-12-04 12:17:07 +0100

    Adjust isFocused to work with new I/O

--- tests/new_io/focus_space.d
+++ tests/new_io/focus_space.d
@@ -39,3 +39,36 @@ unittest {
     assert( root.wasInputHandled);
 
 }
+
+@("Multiple nodes can be focused if they belong to different focus spaces")
+unittest {
+
+    FocusSpace focus1, focus2;
+    Button button1, button2;
+    int one, two;
+
+    auto root = vspace(
+        focus1 = focusSpace(
+            button1 = button("One", delegate { one++; }),
+        ),
+        focus2 = focusSpace(
+            button2 = button("Two", delegate { two++; }),
+        ),
+    );
+
+    root.draw();
+    button1.focus();
+    button2.focus();
+    assert(button1.isFocused);
+    assert(button2.isFocused);
+    assert(cast(Node) focus1.focus == button1);
+    assert(cast(Node) focus2.focus == button2);
+
+    focus1.runInputAction!(FluidInputAction.press);
+    assert(one == 1);
+    assert(two == 0);
+    focus2.runInputAction!(FluidInputAction.press);
+    assert(one == 1);
+    assert(two == 1);
+
+}
