050b5bda91815436b66d7017ed4ce3fff6f79bec
Artha <artha@samerion.com>
2024-12-09 13:23:35 +0100

    Add FindFocusBoxAction
    * And actually define filtering behavior for BranchAction
    * Add `optional`, because it will be *very* handy with publishers

--- source/fluid/future/branch_action.d
+++ source/fluid/future/branch_action.d
@@ -8,6 +8,14 @@ import fluid.types;
 
 abstract class BranchAction : TreeAction {
 
+    private {
+
+        /// Balance is incremented when entering a node, and decremented when leaving. 
+        /// If the balance is negative, the action stops.
+        int _balance;
+
+    }
+
     /// A branch action can only hook to draw calls of specific nodes. It cannot bind into these hooks.
     final override void beforeTree(Node, Rectangle) { }
 
@@ -15,9 +23,62 @@ abstract class BranchAction : TreeAction {
     final override void beforeResize(Node, Vector2) { }
 
     /// ditto
-    final override void afterTree() { }
+    final override void afterTree() {
+        stop;
+    }
 
     /// ditto
     final override void afterInput(ref bool) { }
+
+    /// Branch action excludes the start node from results.
+    /// Returns:
+    ///     True only if the node is a child of the `startNode`; always true if there isn't one set.
+    override bool filterBeforeDraw(Node node) @trusted {
+
+        _balance++;
+
+        const filter = super.filterBeforeDraw(node);
+
+        // Skip the start node
+        if (startNode !is null && node == startNode) {
+            return false;
+        }
+
+        return filter;
+        
+
+    }
+
+    /// Branch action excludes the start node from results.
+    /// Returns:
+    ///     True only if the node is a child of the `startNode`; always true if there isn't one set.
+    override bool filterAfterDraw(Node node) @trusted { 
+
+        const filter = super.filterAfterDraw(node);
+
+        _balance--;
+
+        // Stop if balance is negative
+        if (_balance < 0) {
+            stop;
+            return false;
+        }
+
+        // Stop the action when exiting the start node
+        if (startNode !is null && node == startNode) {
+            stop;
+            return false;
+        }
+
+        return filter;
+
+    }
+
+    override void stopped() {
+
+        super.stopped();
+        _balance = 0;
+
+    }
 
 }
