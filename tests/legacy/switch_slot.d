0d591db1b071582092878d5db3b4908571799093
Artha <artha@samerion.com>
2024-12-23 17:46:15 +0100

    Migrate switch_slot tests

--- tests/legacy/switch_slot.d
+++ tests/legacy/switch_slot.d
@@ -1,10 +1,13 @@
+@Migrated
 module legacy.switch_slot;
 
 import fluid;
+import legacy;
 
 @safe:
 
-@("[TODO] Legacy: SwitchSlot works")
+@("SwitchSlot works")
+@Migrated
 unittest {
 
     Frame bigFrame, smallFrame;
@@ -81,7 +84,8 @@ unittest {
 
 }
 
-@("[TODO] Legacy: Nodes can be moved between SwitchSlots")
+@("Nodes can be moved between SwitchSlots")
+@Migrated
 unittest {
 
     import fluid.frame;
@@ -143,76 +147,5 @@ unittest {
 
     assert(principalDrawn == 1);
     assert(deputyDrawn == 1);
-
-}
-
-@("Deputy SwitchSlot can be placed before its principal")
-unittest {
-
-    import std.algorithm;
-
-    import fluid.space;
-    import fluid.structs;
-
-    SwitchSlot slot;
-
-    auto checker = new class Node {
-
-        Vector2 size;
-        Vector2[] spacesGiven;
-
-        override void resizeImpl(Vector2 space) {
-
-            spacesGiven ~= space;
-            size = minSize = Vector2(500, 200);
-
-        }
-
-        override void drawImpl(Rectangle, Rectangle) {
-
-        }
-
-    };
-
-    auto parentSlot = switchSlot(checker, null);
-    auto childSlot = parentSlot.retry(checker);
-
-    auto root = vspace(
-        layout!"fill",
-        nullTheme,
-
-        // Two slots: child slot that gets resized earlier
-        hspace(
-            layout!"fill",
-            childSlot,
-        ),
-
-        // Parent slot that doesn't give enough space for the child to fit
-        hspace(
-            layout!"fill",
-            vspace(
-                layout!(1, "fill"),
-                parentSlot,
-            ),
-            vspace(
-                layout!(3, "fill"),
-            ),
-        ),
-    );
-
-    root.draw();
-
-    // The principal slot gives the least space, namely the width of the window divided by 4
-    assert(checker.spacesGiven.map!"a.x".minElement == HeadlessBackend.defaultWindowSize.x / 4);
-
-    // The window size that is accepted is equal to its size, as it was assigned by the fallback slot
-    assert(checker.spacesGiven[$-1] == checker.size);
-
-    // A total of three resizes were performed: one by the fallback, one by the parent and one, final, by the parent
-    // using previous parameters
-    assert(checker.spacesGiven.length == 3);
-
-    // The first one (which should be the child's) has the largest width given, equal to the window width
-    assert(checker.spacesGiven[0].x == HeadlessBackend.defaultWindowSize.x);
 
 }
