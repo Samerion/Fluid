d930bc3134458396a266dccbcc7cad7bb129d65e
Artha <artha@samerion.com>
2024-12-23 18:04:46 +0100

    Migrate popup_button tests
    I'm not sure how does this work but I'll figure out later

---
+++ tests/nodes/popup_button.d
@@ -0,0 +1,85 @@
+module nodes.popup_button;
+
+import fluid;
+import fluid.future.pipe;
+
+@safe:
+
+@("PopupButton supports keyboard navigation")
+unittest {
+
+    import fluid.backend;
+
+    string lastAction;
+
+    void action(string text)() {
+        lastAction = text;
+    }
+
+    Button[6] buttons;
+
+    auto main = popupButton("Options",
+        buttons[0] = button("Edit", &action!"edit"),
+        buttons[1] = button("Copy", &action!"copy"),
+        buttons[2] = popupButton("Share",
+            buttons[3] = button("SMS", &action!"sms"),
+            buttons[4] = button("Via e-mail", &action!"email"),
+            buttons[5] = button("Send to device", &action!"device"),
+        ),
+    );
+    auto focus = focusSpace(nullTheme, main);
+    auto root = focus;
+
+    auto sharePopupButton = cast(PopupButton) buttons[2];
+    auto sharePopup = sharePopupButton.popup;
+
+    root.draw();
+
+    // Focus the button
+    focus.focusNext()
+        .thenAssertEquals(main)
+
+        // Press it
+        .then(() => focus.runInputAction!(FluidInputAction.press))
+        .then(_ => root.nextFrame)
+        
+        // A popup should open
+        .then(() => assert(focus.isFocused(buttons[0]), "The first button inside should be focused"))
+
+        // Go to the previous button, expecting wrap
+        .then(() => focus.runInputAction!(FluidInputAction.focusPrevious))
+        .then(_ => root.nextFrame)
+        .then(() => assert(focus.isFocused(buttons[2]), "The last button inside the first menu should be focused"))
+
+        // Press the last button
+        .then(() => buttons[2].press())
+        .then(() => root.nextFrame)
+        .then(() => assert(focus.isFocused(buttons[3]), "The first button of the second menu should be focused"))
+
+        // The up arrow should do nothing
+        .then(() => focus.runInputAction!(FluidInputAction.focusUp))
+        .then(_ => root.nextFrame)
+        .then(() => assert(focus.isFocused(buttons[3])))
+
+        // Press the down arrow
+        .then(() => focus.runInputAction!(FluidInputAction.focusDown))
+        .then(_ => root.nextFrame)
+        .then(() => assert(focus.isFocused(buttons[4])))
+
+        // Press the button
+        .then(() => buttons[4].press)
+        .then(() => assert(focus.isFocused(buttons[4])))
+        .then(() => assert(lastAction == "email"))
+        .then(() => assert(!sharePopup.isHidden))
+
+        // Close the popup
+        .then(() => focus.runInputAction!(FluidInputAction.cancel))
+
+        // Need two frames to process the tree action
+        .then(_ => main.nextFrame)
+        .then(() => main.nextFrame)
+
+        // Need another frame for the tree action
+        .then(() => assert(sharePopup.isHidden));
+
+}
