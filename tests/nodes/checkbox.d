1b2c1515e83a204e3c8b7ab3af6b2eb6b4311f85
Artha <artha@samerion.com>
2024-12-31 23:02:31 +0100

    Add CanvasIO support to Checkbox

--- tests/nodes/checkbox.d
+++ tests/nodes/checkbox.d
@@ -24,3 +24,37 @@ unittest {
     assert(!root.isChecked);
 
 }
+
+@("Checkbox draws a checkmark when pressed")
+unittest {
+
+    auto theme = Theme(
+        rule!Checkbox(
+            Rule.margin = 0,
+            Rule.border = 1,
+            Rule.padding = 0,
+            Rule.borderStyle = colorBorder(color("#222")),
+        ),
+    );
+    auto input = checkbox();
+    auto test = testSpace(theme, input);
+    input.size = Vector2(10, 10);
+
+    test.drawAndAssert(
+        input.drawsRectangle().ofColor("#222"),
+        input.drawsRectangle().ofColor("#222"),
+        input.drawsRectangle().ofColor("#222"),
+        input.drawsRectangle().ofColor("#222"),
+        input.drawsImage(Image.init),
+    );
+    assert(input.markImage == Image.init);
+
+    input.toggle();
+    test.draw(),
+    test.drawAndAssert(
+        input.drawsRectangle().ofColor("#222"),
+        input.drawsImage(input.markImage),
+    );
+    assert(input.markImage != Image.init);
+
+}
