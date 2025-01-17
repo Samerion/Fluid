e77fde98cca08896740a875a4d879599aba731aa
Artha <artha@samerion.com>
2025-01-17 06:10:30 +0100

    Migrate tint and readme tests

--- source/fluid/style.d
+++ source/fluid/style.d
@@ -594,6 +594,7 @@ unittest {
 
 }
 
+@("Legacy: Style.tint stacks")
 unittest {
 
     import fluid.frame;
@@ -634,6 +635,7 @@ unittest {
 
 }
 
+@("Legacy: Border occupies and takes space")
 unittest {
 
     import fluid.frame;
