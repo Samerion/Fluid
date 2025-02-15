db8206a1d86818465d76b315810f4459fc72a8e8
Artha <artha@samerion.com>
2025-02-15 11:53:20 +0100

    Define the FloatSizeLimit node property

--- source/fluid/size_lock.d
+++ source/fluid/size_lock.d
@@ -5,6 +5,10 @@
 /// to display correctly. If their `Node.layout.align` property is set to `fill`, they will
 /// instead attempt to use all of the space they're given. Using `SizeLock` allows
 /// for a compromise by placing a limit on how much space a node can use.
+///
+/// Note:
+///     Using `layout!"fill"` with `SizeLock` will negate the lock's effect. Use a different
+///     alignment like `"start"`, `"center"` or `"end"`.
 module fluid.size_lock;
 
 /// Size limit can be used to center content on a wide screen. By using `sizeLock!node`,
@@ -42,10 +46,12 @@ import fluid.backend;
 /// `sizeLimit` can only be used with `SizeLock`.
 ///
 /// Params:
-///     x = Maximum width of the node.
-///     y = Maximum height of the node.
+///     x = Maximum width of the node in pixels.
+///     y = Maximum height of the node in pixels.
 /// Returns:
 ///     A configured node parameter struct, which can be passed into the `sizeLock` node builder.
+///     This will be a `SizeBounds` struct if the input parameters are `float` (preferred),
+///     or `SizeLimit` if they are `size_t` like `uint` or `ulong`.
 SizeLimit sizeLimit(size_t x, size_t y) {
     return SizeLimit(x, y);
 }
@@ -60,6 +66,27 @@ SizeLimit sizeLimitY(size_t y) {
     return SizeLimit(0, y);
 }
 
+/// ditto
+FloatSizeLimit sizeLimit(float x, float y) {
+    return FloatSizeLimit(
+        Vector2(x, y)
+    );
+}
+
+/// ditto
+FloatSizeLimit sizeLimitX(float x) {
+    return FloatSizeLimit(
+        Vector2(x, float.infinity)
+    );
+}
+
+/// ditto
+FloatSizeLimit sizeLimitY(float y) {
+    return FloatSizeLimit(
+        Vector2(float.infinity, y)
+    );
+}
+
 struct SizeLimit {
 
     size_t x;
@@ -67,6 +94,35 @@ struct SizeLimit {
 
     void apply(T)(SizeLock!T node) {
         node.limit = this;
+    }
+
+}
+
+/// This node property defines the maximum size for a `SizeLock` node. Nodes can be given a limit
+/// by setting either `width` or `height`.
+///
+/// The `init` value defaults to no restrictions.
+struct FloatSizeLimit {
+
+    /// The imposed limit as a vector. The `x` field is the maximum width,
+    /// and `y` is the maximum height. They both default to `infinity`, effectively not
+    /// setting any limit.
+    auto limit = Vector2(float.infinity, float.infinity);
+
+    /// Returns:
+    ///     The maximum width imposed on the node.
+    ref inout(float) width() inout return {
+        return limit.x;
+    }
+
+    /// Returns:
+    ///     The maximum height imposed on the node.
+    ref inout(float) height() inout return {
+        return limit.y;
+    }
+
+    void apply(T)(SizeLock!T node) {
+        node.sizeLimit = this;
     }
 
 }
@@ -86,27 +142,35 @@ class SizeLock(T : Node) : T {
 
     /// The maximum size of this node.
     /// If a value on either axis is `0`, limit will not be applied on the axis.
+    ///
+    /// `limit` has been superseded by `sizeLimit`, which uses floats instead of integers, like
+    /// the rest of Fluid. For now, it takes priority over `sizeLimit` if set.
     SizeLimit limit;
 
+    /// The maximum size of the node.
+    FloatSizeLimit sizeLimit;
+
     this(T...)(T args) {
-
         super(args);
-        this.limit = limit;
-
     }
 
     override void resizeImpl(Vector2 space) {
 
         // Virtually limit the available space
         if (limit.x != 0) space.x = min(space.x, limit.x);
+        else space.x = min(space.x, sizeLimit.width);
+
         if (limit.y != 0) space.y = min(space.y, limit.y);
+        else space.y = min(space.y, sizeLimit.height);
 
         // Resize the child
         super.resizeImpl(space);
 
         // Apply the limit to the resulting value; fill in remaining space if available
         if (limit.x != 0) minSize.x = max(space.x, min(limit.x, minSize.x));
+        else minSize.x = max(space.x, min(sizeLimit.width, minSize.y));
         if (limit.y != 0) minSize.y = max(space.y, min(limit.y, minSize.y));
+        else minSize.y = max(space.y, min(sizeLimit.height, minSize.y));
 
     }
 
