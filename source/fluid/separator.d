06fc23db67b5ddc0e2d33bc0fb88f690d16022e7
Artha <artha@samerion.com>
2025-03-29 00:00:23 +0100

    Improve separator documentation

--- source/fluid/separator.d
+++ source/fluid/separator.d
@@ -1,5 +1,21 @@
-///
+/// A line to separate unrelated nodes, or to divide content into groups.
 module fluid.separator;
+
+@safe:
+
+/// Use [hseparator] to create horizontal lines, and [vseparator] to create vertical lines.
+@("Separator reference example")
+unittest {
+    import fluid.space;
+    import fluid.label;
+    run(
+        vspace(
+            label("Hello"),
+            hseparator(),
+            label("Goodbye"),
+        ),
+    );
+}
 
 import fluid.node;
 import fluid.utils;
@@ -8,10 +24,8 @@ import fluid.structs;
 
 import fluid.io.canvas;
 
-@safe:
-
-
-/// A separator node creates a line, used to separate unrelated parts of content.
+/// A [node builder][nodeBuilder] for [Separator][Separator]. The `vseparator` creates a vertical
+/// line, while `hseparator` creates a horizontal one.
 enum vseparator = NodeBuilder!(Separator, (a) {
 
     a.isHorizontal = false;
@@ -27,13 +41,14 @@ enum hseparator = NodeBuilder!(Separator, (a) {
 
 }).init;
 
-/// ditto
+/// A separator node draws a vertical or horizontal line to separate content.
 class Separator : Node {
 
     CanvasIO canvasIO;
 
     public {
 
+        /// If true, separator draws a horizontal line, otherwise it draws a vertical one.
         bool isHorizontal;
 
     }
