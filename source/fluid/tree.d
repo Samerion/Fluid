61eddae622ecf8afc54568380ca595868d8b3f7e
Artha <artha@samerion.com>
2025-12-20 19:20:11 +0100

    Merge fluid.future.branch_action into fluid.tree

--- source/fluid/tree.d
+++ source/fluid/tree.d
@@ -273,3 +273,87 @@ abstract class TreeAction : Publisher!() {
     }
 
 }
+
+abstract class BranchAction : TreeAction {
+
+    private {
+
+        /// Balance is incremented when entering a node, and decremented when leaving.
+        /// If the balance is negative, the action stops.
+        int _balance;
+
+    }
+
+    /// A branch action can only hook to draw calls of specific nodes. It cannot bind into these hooks.
+    final override void beforeTree(Node, Rectangle) { }
+
+    /// ditto
+    final override void beforeResize(Node, Vector2) { }
+
+    /// ditto
+    final override void afterTree() {
+        stop;
+    }
+
+    /// Branch action excludes the start node from results.
+    /// Returns:
+    ///     True only if the node is a child of the `startNode`; always true if there isn't one set.
+    override bool filterBeforeDraw(Node node) @trusted {
+
+        _balance++;
+
+        // No start node
+        if (startNode is null) {
+            return true;
+        }
+
+        const filter = super.filterBeforeDraw(node);
+
+        // Skip the start node
+        if (node == startNode) {
+            return false;
+        }
+
+        return filter;
+
+
+    }
+
+    /// Branch action excludes the start node from results.
+    /// Returns:
+    ///     True only if the node is a child of the `startNode`; always true if there isn't one set.
+    override bool filterAfterDraw(Node node) @trusted {
+
+        _balance--;
+
+        // Stop if balance is negative
+        if (_balance < 0) {
+            stop;
+            return false;
+        }
+
+        // No start node
+        if (startNode is null) {
+            return true;
+        }
+
+        const filter = super.filterAfterDraw(node);
+
+        // Stop the action when exiting the start node
+        if (node == startNode) {
+            stop;
+            return false;
+        }
+
+        return filter;
+
+    }
+
+    override void stopped() {
+
+        super.stopped();
+        _balance = 0;
+
+    }
+
+}
